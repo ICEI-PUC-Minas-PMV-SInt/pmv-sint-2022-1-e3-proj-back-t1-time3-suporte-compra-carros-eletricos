aafcd55cb9d0e45b84f854bf7109ba6d33b687ac
Iara000 <81269914+Iara000@users.noreply.github.com>
2022-05-25 14:56:09 -0300

    Create ApplicationDbContext.cs

---
+++ src/eletric-app/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace eletric_app.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
