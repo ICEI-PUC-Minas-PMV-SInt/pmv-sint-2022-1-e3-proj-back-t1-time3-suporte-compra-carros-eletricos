e885909d06d768124bd36ceeb4b8fb5a2e9ee042
Iara000 <iaracamargo000@gmail.com>
2022-06-01 16:47:24 -0300

    Criando database VeiculoC

--- src/eletric-app/Migrations/20220601194412_M00.Designer.cs
+++ src/eletric-app/Migrations/20220601194412_M00.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eletric_app.Models;
 
 namespace eletric_app.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220601194412_M00")]
+    partial class M00
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/eletric-app/Models/VeiculoC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eletric_app.Models
+{
+    [Table("VeiculosC")]
+    public class VeiculoC
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o modelo!")]
+        public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o motor!")]
+        public string Motor { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        [Required(ErrorMessage = "Obrigatório informar a média de consumo!")]
+        public decimal Media { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Valor { get; set; }
+
+        public DateTime Data { get; set; }
+    }
+}
